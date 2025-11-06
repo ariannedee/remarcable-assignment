27286737109fd764766f3c504c2b2a558b5b8529
ariannedee <ariannedee@gmail.com>
2025-11-05 22:28:27 -0800

    Handle category filter

--- remarcable_project/apps/products/views.py
+++ remarcable_project/apps/products/views.py
@@ -6,11 +6,15 @@ from .models import Category, Product, Tag
 def index(request):
     filter_dict = request.GET
     search = filter_dict.get('search')
+    category = filter_dict.get('category')
 
     qs = Product.objects.all()
 
     if search:
         qs = qs.filter(name__contains=search)
+
+    if category:
+        qs = qs.filter(category__pk=category)
 
     context = {
         "categories": Category.objects.all(),
